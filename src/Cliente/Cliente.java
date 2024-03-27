395ad792f283c9f29b34e90b149f56d134fc89df
aninha1103 <amcastroesousa@gmail.com>
2024-03-26 22:05:48 -0300

    getter e setter

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -6,4 +6,47 @@ public class Cliente {
     private String tipo;
     private String documento;
     private float saldo;
+
+    public Cliente(int id, String tipo, String documento, float saldo) {
+        this.id = id;
+        this.tipo = tipo;
+        this.documento = documento;
+        this.saldo = (float) 0.0;
+    }
+
+    public Cliente() {
+    }  
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
 }
---
+++ src/Main/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+import java.util.Scanner;
+        
+public class Main {
+    public static void main(String[] args) {
+        Scanner Cliente = new Scanner(System.in);
+        System.out.println("");
+    }
+}
