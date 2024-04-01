792b90fbf414db83519d17e234a3af2c19cf7169
JoaoPolar <jn459998@gmail.com>
2024-04-01 19:47:14 -0300

    Utilizando herança

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -1,52 +1,18 @@
 
 package Cliente;
 
-public class Cliente {
+public abstract class Cliente {
+    
     private int id;
     private String tipo;
     private String documento;
     private float saldo;
+}
 
-    public Cliente(int id, String tipo, String documento, float saldo) {
-        this.id = id;
-        this.tipo = tipo;
-        this.documento = documento;
-        this.saldo = (float) 0.0;
-    }
+class ContaPF extends Cliente {
+    private int CPF;
+}
 
-    public Cliente() {
-    }  
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public float getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
-    
+class ContaPJ extends Cliente {
+    private int CNPJ;
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,21 +1,20 @@
-package trabalho;
-    
 import java.util.Scanner;
 
-public class Trabalho {
-        
-public class Main {
+public class Menu {
     
         public static void main(String[] args) {
             Scanner Entrada = new Scanner(System.in);
 
-            System.out.println("1 - Criar conta PJ\n2 - Criar Conta PF");
+            System.out.println("1 - Criar conta\n2 - Listar cinta");
             int valor = Entrada.nextInt();
+            
             switch (valor) {
                 case 1 -> System.out.println("Teste");
+                
                 case 2 -> System.out.println("Teste 2");
+                
                 default -> System.out.println("Selecione um valor valido");
+                
             }
         }
-    }
 }
