032f7f3336a7a1b281cc4f319147f75187840e1d
Laboratorio <Laboratorio@173.2.5.226>
2024-04-08 21:48:18 -0300

    Alteração no menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,5 @@
+import Cliente.ContaPF;
+import Cliente.ContaPJ;
 import java.util.Scanner;
 
 public class Menu {
@@ -10,11 +12,47 @@ public class Menu {
             int valor = Entrada.nextInt();
             
             switch (valor) {
-                case 1 -> System.out.println("Teste");
+                case 1:
+                    
+                    System.out.println("digite um id:");
+                    int valor_id = Entrada.nextInt();
+                    
+                    Entrada.nextLine();
+                    System.out.println("Digite o nome da conta:");
+                    String valor_nome = Entrada.nextLine();
+                    
+                    System.out.println("digite o tipo da Conta (ContaPJ ou ContaPF)");
+                    String valor_tipo = Entrada.nextLine();
+                    
+                    if ("ContaPJ".equals(valor_tipo)){
+                        
+                        System.out.println("digite o CNPJ");
+                        int valor_CNPJ = Entrada.nextInt();
+                        
+                        ContaPJ teste = new ContaPJ(valor_id, valor_nome, valor_tipo, valor_CNPJ);
+                        System.out.println("Conta criada");
+                        
+                    }else {
+                        
+                        System.out.println("digite o CPF");
+                        int valor_CPF = Entrada.nextInt();
+                        
+                        ContaPF conta = new ContaPF(valor_id,valor_nome, valor_tipo, valor_CPF);
+                        System.out.println("Conta criada");
+                    }
+                break;
+                    
+                case 2:
+                    
+                break;
                 
-                case 2 -> System.out.println("Teste 2");
+                case 3:
+                    
+                break;
                 
-                default -> System.out.println("Selecione um valor valido");
+                default:
+                    
+                    System.out.println("Selecione um valor valido");
                 
             }
         }
